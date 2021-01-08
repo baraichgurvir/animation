4520da5263589ee6b3391495865c1f1fb3708cdd
baraichgurvir <49345450+baraichgurvir@users.noreply.github.com>
2021-01-08 13:40:05 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -12,23 +12,3 @@ if (document.getElementsByTagName('animscript').length === 0) {
 } else {
     console.error("Error: Duplicates Avaliable")
 }
-
-childs = [];
-childs = document.body.children;
-
-if (childs[childs.length - 2].getAttribute("src") === null || childs[childs.length - 2].getAttribute("src") != "auth.js") {
-    console.error("Error: script tag with src=\"auth.js\" should be at the end of body")
-    for (var child = 0; child < childs.length; child++) {
-        document.body.removeChild(childs[child]);
-    }
-} else if (childs[childs.length - 3].getAttribute("src") === null || childs[childs.length - 3].getAttribute("src") != "animations.js") {
-    console.error("Error: script tag with src=\"animations.js\" should be before auth.js and after animscript")
-    for (var child = 0; child < childs.length; child++) {
-        document.body.removeChild(childs[child]);
-    }
-} else if (childs[childs.length - 5].getAttribute("src") === null || childs[childs.length - 5].getAttribute("src") != "animation.js") {
-    console.error("Error: script tag with src=\"animation.js\" should be before animscript")
-    for (var child = 0; child < childs.length; child++) {
-        document.body.removeChild(childs[child]);
-    }
-}
